6826299aebcc2f148e52c16683c77c36b715c9c0
Samuel Funk <sfunk02@ccs.local>
2022-03-25 09:51:41 -0400

    added rgbMain

--- rgbMain.py
+++ rgbMain.py
@@ -1,22 +1,53 @@
 import time
 import board
-from rgb import LED
+from rgb import RGB
 
-rPin = board.D8
-gPin = board.D9
-bPin = board.D10
+r1 = board.D8
+g1 = board.D9
+b1 = board.D10
+r2 = board.D4
+g2 = board.D5
+b2 = board.D7
 
-rPin2 = board.D4
-gPin2 = board.D5
-bPin2 = board.D7
+full = 65535
+half = int(65535/2)
+fast = 0.5
+slow = 2
 
-rgbLED = LED(rPin, gPin, bPin)
-rgbLED2 = LED(rPin2, gPin2, bPin2)
+myRGBled1 = RGB(r1, g1, b1)
+myRGBled2 = RGB(r2, g2, b2)
+
 
 while True:
-    rgbLED.off()
-    rgbLED2.off()
-    rgbLED.fade()
-    rgbLED.off()
-    rgbLED2.fade()
-    rgbLED2.off()
+    '''Shines two RGB LEDs in opposing colors, then rainbows!'''
+    myRGBled1.blue(half)
+    myRGBled2.yellow(half)
+    time.sleep(1)
+    myRGBled1.off()
+    myRGBled2.off()
+    time.sleep(1)
+
+    myRGBled1.red()
+    myRGBled2.cyan()
+    time.sleep(1)
+    myRGBled1.off()
+    myRGBled2.off()
+    time.sleep(1)
+
+    myRGBled1.green()
+    myRGBled2.magenta()
+    time.sleep(1)
+    myRGBled1.off()
+    myRGBled2.off()
+    time.sleep(1)
+
+    myRGBled1.Blinky(fast) #higher rate is slower
+    myRGBled2.Blinky(fast)
+    time.sleep(1)
+    myRGBled1.off()
+    myRGBled2.off()
+    time.sleep(1)
+
+    myRGBled1.rainbow()
+    myRGBled2.rainbow()
+    time.sleep(1)
